7fcce5a39607cebf6bb999b22e62ab86c5815c63
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-08-13 14:53:03 -0400

    Editar lo de expresión diferencial después de enseñar esa parte en el mini curso.

--- 12-expresion_diferencial.R
+++ 12-expresion_diferencial.R
@@ -1,4 +1,4 @@
-## ----cargar_paquetes, message = FALSE-------------------------------------------------------
+## ----cargar_paquetes, message = FALSE-----------------------------------
 ## Paquetes de este capítulo
 library("MouseGastrulationData") ## para descargar datos de ejemplo
 library("scater") ## para gráficas y control de calidad
@@ -10,7 +10,7 @@ library("bluster") ## métodos de clustering
 library("edgeR") ## para expresión diferencial
 
 
-## ---- echo = FALSE--------------------------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------
 set.seed(20210811)
 knitr::kable(data.frame(
     gene = paste0("gene", rep(rep(1:2, each = 2), 2)),
@@ -20,7 +20,7 @@ knitr::kable(data.frame(
 ))
 
 
-## ---- echo = FALSE--------------------------------------------------------------------------
+## ---- echo = FALSE------------------------------------------------------
 set.seed(20210811)
 knitr::kable(data.frame(
     condición = paste0("grupo", rep(1:2, 2)),
@@ -29,14 +29,14 @@ knitr::kable(data.frame(
 ))
 
 
-## ---- message = FALSE-----------------------------------------------------------------------
+## ---- message = FALSE---------------------------------------------------
 #--- loading ---#
 library("MouseGastrulationData")
 sce.chimera <- WTChimeraData(samples = 5:10)
 sce.chimera
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## Exploremos los datos
 sapply(colData(sce.chimera)[, -(1:2)], function(x) {
     x <- if (is.character(x) || is.integer(x)) factor(x) else x
@@ -44,7 +44,7 @@ sapply(colData(sce.chimera)[, -(1:2)], function(x) {
 })
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## Número de células en nuestras variables principales
 with(colData(sce.chimera), table(sample, pool, tomato))
 
@@ -52,7 +52,7 @@ with(colData(sce.chimera), table(sample, pool, tomato))
 length(unique(sce.chimera$celltype.mapped))
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 #--- feature-annotation ---#
 library("scater")
 rownames(sce.chimera) <- uniquifyFeatureNames(
@@ -95,7 +95,7 @@ merged <- runTSNE(merged, dimred = "corrected", external_neighbors = TRUE)
 merged <- runUMAP(merged, dimred = "corrected", external_neighbors = TRUE)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## Clusters vs DE por td-Tomato
 table(colLabels(merged), merged$tomato)
 
@@ -103,13 +103,27 @@ table(colLabels(merged), merged$tomato)
 table(colLabels(merged), merged$pool)
 
 
-## ---- fig.width=10--------------------------------------------------------------------------
+## ---- fig.width=10------------------------------------------------------
 library("patchwork")
 plotTSNE(merged, colour_by = "tomato", text_by = "label") +
     plotTSNE(merged, colour_by = data.frame(pool = factor(merged$pool)))
 
 
-## ---- fig.width=10, warning = FALSE---------------------------------------------------------
+## ---- fig.width=10------------------------------------------------------
+plotTSNE(merged,
+    colour_by = "tomato",
+    other_fields = c("tomato")
+) + facet_wrap(~tomato)
+
+
+## ---- fig.width=15------------------------------------------------------
+plotTSNE(merged,
+    colour_by = data.frame(pool = factor(merged$pool)),
+    other_fields = c("pool")
+) + facet_wrap(~pool)
+
+
+## ---- fig.width=10, warning = FALSE-------------------------------------
 ## Definir colores, si no scater nos los pone en una escala
 ## continua
 cols_label <- Polychrome::palette36.colors(length(unique(merged$label)))
@@ -127,7 +141,7 @@ plotTSNE(merged, colour_by = "label", text_by = "label") +
     scale_colour_manual(values = cols_celltype.mapped)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 library("bluster")
 pairwiseRand(colLabels(merged), merged$celltype.mapped, "index")
 
@@ -135,7 +149,7 @@ by.label <- table(colLabels(merged), merged$celltype.mapped)
 pheatmap::pheatmap(log2(by.label + 1), color = viridis::viridis(101))
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 # Using 'label' and 'sample' as our two factors; each column of the output
 # corresponds to one unique combination of these two factors.
 summed <- aggregateAcrossCells(merged,
@@ -149,62 +163,68 @@ dim(summed)
 with(colData(merged), length(unique(celltype.mapped)) * length(unique(sample)))
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 label <- "Mesenchyme"
 current <- summed[, label == summed$celltype.mapped]
 dim(current)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 # Creating up a DGEList object for use in edgeR:
 library("edgeR")
 y <- DGEList(counts(current), samples = colData(current))
 y
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 discarded <- current$ncells < 10
 y <- y[, !discarded]
 summary(discarded)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 keep <- filterByExpr(y, group = current$tomato)
 y <- y[keep, ]
 summary(keep)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 y <- calcNormFactors(y)
 y$samples
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 par(mfrow = c(2, 3))
 for (i in seq_len(ncol(y))) {
     plotMD(y, column = i)
 }
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 par(mfrow = c(1, 1))
 plotMDS(cpm(y, log = TRUE),
     col = ifelse(y$samples$tomato, "red", "blue")
 )
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
+plotMDS(cpm(y, log = TRUE),
+    col = c("3" = "darkorchid1", "4" = "darkblue", "5" = "tomato4")[factor(y$samples$pool)]
+)
+
+
+## -----------------------------------------------------------------------
 design <- model.matrix(~ factor(pool) + factor(tomato), y$samples)
 design
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 if (interactive()) {
     ExploreModelMatrix::ExploreModelMatrix(y$samples[, c("pool", "tomato")], ~ factor(pool) + factor(tomato))
 }
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 y <- estimateDisp(y, design)
 summary(y$trended.dispersion)
 
@@ -216,14 +236,15 @@ summary(fit$df.prior)
 plotQLDisp(fit)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 res <- glmQLFTest(fit, coef = ncol(design))
-summary(decideTests(res))
+de_n <- summary(decideTests(res))
+de_n
 
 topTags(res)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 # Removing all pseudo-bulk samples with 'insufficient' cells.
 summed.filt <- summed[, summed$ncells >= 10]
 
@@ -238,7 +259,7 @@ class(de.results)
 length(de.results)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 cur.results <- de.results[["Allantois"]]
 cur.results[order(cur.results$PValue), ]
 
@@ -246,26 +267,26 @@ y.allantois <- metadata(cur.results)$y
 plotBCV(y.allantois)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 metadata(de.results)$failed
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 cur.results.Mesenchyme <- de.results[["Mesenchyme"]]
 y.Mesenchyme <- metadata(cur.results.Mesenchyme)$y
 plotBCV(y.Mesenchyme)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 colData(summed)[1, ]
 which.max(counts(summed)[, 1])
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 counts(summed)[which.max(counts(summed)[, 1]), 1]
 
 
-## ----volcano, echo = FALSE------------------------------------------------------------------
+## ----volcano, echo = FALSE----------------------------------------------
 library("ggplot2")
 ## class(cur.results.Mesenchyme)
 # [1] "DFrame"
@@ -273,17 +294,20 @@ library("ggplot2")
 # [1] "S4Vectors"
 df <- as.data.frame(cur.results.Mesenchyme)
 df$label <- rownames(df)
-p <- ggplot(df,
+p <- ggplot(
+    df,
     aes(
         x = logFC,
         y = -log10(PValue),
         color = FDR < 0.05,
         label = label
-    )) + geom_point()
+    )
+) +
+    geom_point()
 p
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## Valor esperado
 counts(summed)[which.max(counts(summed)[, 1]), 1]
 
@@ -300,7 +324,7 @@ sum(counts(merged)[
 ])
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 library("ggplot2")
 ## class(cur.results.Mesenchyme)
 # [1] "DFrame"
@@ -308,48 +332,51 @@ library("ggplot2")
 # [1] "S4Vectors"
 df <- as.data.frame(cur.results.Mesenchyme)
 df$label <- rownames(df)
-p <- ggplot(df,
+p <- ggplot(
+    df,
     aes(
         x = logFC,
         y = -log10(PValue),
         color = FDR < 0.05,
         label = label
-    )) + geom_point()
+    )
+) +
+    geom_point()
 p
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 p + ggrepel::geom_text_repel(data = subset(df, FDR < 0.05))
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 plotly::ggplotly(p)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 table(merged$sample)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 abundances <- table(merged$celltype.mapped, merged$sample)
 abundances <- unclass(abundances)
 head(abundances)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 # Attaching some column metadata.
 extra.info <- colData(merged)[match(colnames(abundances), merged$sample), ]
 y.ab <- DGEList(abundances, samples = extra.info)
 y.ab
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 keep <- filterByExpr(y.ab, group = y.ab$samples$tomato)
 y.ab <- y.ab[keep, ]
 summary(keep)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 design <- model.matrix(~ factor(pool) + factor(tomato), y.ab$samples)
 design
 
@@ -359,21 +386,21 @@ summary(y.ab$common.dispersion)
 plotBCV(y.ab, cex = 1)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 fit.ab <- glmQLFit(y.ab, design, robust = TRUE, abundance.trend = FALSE)
 summary(fit.ab$var.prior)
 
 plotQLDisp(fit.ab, cex = 1)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 res <- glmQLFTest(fit.ab, coef = ncol(design))
 summary(decideTests(res))
 
 topTags(res)
 
 
-## -------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------
 ## Información de la sesión de R
 Sys.time()
 proc.time()
